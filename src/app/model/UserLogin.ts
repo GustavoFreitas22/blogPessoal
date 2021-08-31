d1475b3142c38047c173c1202aa475efc5b76a1a
GustavoFreitas22 <gusfmotta@gmail.com>
2021-08-31 09:27:32 -0300

    Consumo de API - Security

--- src/app/model/UserLogin.ts
+++ src/app/model/UserLogin.ts
@@ -1,7 +1,7 @@
 export class UserLogin{
     public id: number
     public nome: string
-    public usuario: string
+    public userName: string
     public senha: string
     public foto: string
     public tipo: string
